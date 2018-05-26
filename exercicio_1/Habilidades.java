846e7ab0e31393dbf8b5b40973a045d4f0e7cfe6
Victor Henrique <vhenrique21@icloud.com>
2018-05-26 00:19:29 -0300

    Alteração nas Habilidades.
    Versão final das Habilidades, sujeito a alteração de dano apenas, para
    tornar a simulação mais dinâmica.

--- exercicio_1/Habilidades.java
+++ exercicio_1/Habilidades.java
@@ -1,50 +1,20 @@
 package exercicio_1;
 
-public class Habilidades {
+import java.util.Random;
 
-	private int valor;
-	private int dano;
-	private String nomeDaHabilidade;
-	// tipo: 0 = Jedi e 1 = Sith
-	// tipoDeDominioDaHabilidade: 0 = Forca e 1 = Sabre
+public class NomesDeHabilidades {
+	
+	private static int valor;
+	static Random geradorNumeros = new Random();
 
-	public int getDano() {
-		return dano;
-	}
+	public static String []habilidadeJediForca = {"FORCE PUSH", "JEDI MIND TRICK", "FORCE THROW", "FORCE HEALING", "FORCE ENHANCEMENT"};
+	public static String []habilidadeJediSabre = {"ATAQUE FRACO", "ATAQUE NORMAL", "ATAQUE FORTE"};
+	public static String []habilidadeSithForca = {"FORCE CHOKE", "FORCE LIGHTNING", "FORCE THROW", "FORCE HEALING", "FORCE ENHANCEMENT" };
+	public static String []habilidadeSithSabre = {"ATAQUE FRACO", "ATAQUE NORMAL", "ATAQUE FORTE"};
+	
+	public static int []habilidadeJediForcaDano = {5, 3, 6, 2, 1};
+	public static int []habilidadeJediSabreDano = {5, 8, 12};
+	public static int []habilidadeSithForcaDano = {5, 6, 4, 1, 2};
+	public static int []habilidadeSithSabreDano = {5, 8, 12};
 
-	public void setDano(int dano) {
-		this.dano = dano;
-	}
-
-	public String getNomeDaHabilidade() {
-		return nomeDaHabilidade;
-	}
-
-	public void setNomeDaHabilidade(String nomeDaHabilidade) {
-		this.nomeDaHabilidade = nomeDaHabilidade;
-	}
-
-	public void habilidade(int tipo, int tipoDeDominioDaHabidade) {
-
-		valor = NomesDeHabilidades.habilidades();
-		if (tipo == 0) {
-			if (tipoDeDominioDaHabidade == 0) {
-				nomeDaHabilidade = NomesDeHabilidades.habilidadeJediForca[valor];
-				dano = NomesDeHabilidades.habilidadeJediForcaDano[valor];
-			} else if (tipoDeDominioDaHabidade == 1) {
-				nomeDaHabilidade = NomesDeHabilidades.habilidadeJediSabre[valor];
-				dano = NomesDeHabilidades.habilidadeJediSabreDano[valor];
-			}
-		}
-
-		if (tipo == 1) {
-			if (tipoDeDominioDaHabidade == 0) {
-				nomeDaHabilidade = NomesDeHabilidades.habilidadeSithForca[valor];
-				dano = NomesDeHabilidades.habilidadeSithForcaDano[valor];
-			} else if (tipoDeDominioDaHabidade == 1) {
-				nomeDaHabilidade = NomesDeHabilidades.habilidadeSithSabre[valor];
-				dano = NomesDeHabilidades.habilidadeSithSabreDano[valor];
-			}
-		}
-	}
 }
